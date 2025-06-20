4ef2bb95303502b7d6e956ace28812eb7862442a
Teoslip <tjachiet@gmail.com>
2025-06-20 21:54:50 +0200

    Change game logic
    I was way off in my use of module patterns. I just need functions
    for now. I also don't need a Player function factory, i just need
    2 players that i can make directly in the gameflow function. The article
    for the connect four game really gave me a nice roadmap that i can trust
    and now i can build over this basis with confidence.

--- script.js
+++ script.js
@@ -1,37 +1,94 @@
-const Gameboard = (function () {
-    const board = [['', '', ''],['', '', ''],['', '', '']];
+function GameBoard() {
+    const boardSize = 3;
+    const board = [];
 
-    const drawBoard = () => {
+    //Making the gameBoard 2D Array
+    for (let i = 0; i < boardSize; i++) {
+        board[i] = []; //Making rows
+        for (let j = 0; j < boardSize; j++) {
+            board[i].push(Cell()); // TODO I have to make a cell object now
+        }
+    }
 
+    const fillCell = (token, row, column) => {
+        if (board[column][row].getValue() !== 0) { //Checking if the cell is empty
+            console.log('cell already taken');
+            return;
+        } else {
+            board[row][column].addToken(token);
+        }
     };
 
-    const updateBoard = (Player, location) => {
+    const getBoard = () => board;
 
+    const displayBoard = () => {
+        const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardValues);
     };
 
-    return {board};
-})();
+    return { getBoard, fillCell, displayBoard };
+};
 
-const GameFlow = (function () {
-    const turn = () => {
-        console.log('whos turn is it ?');
+function Cell() {
+    //Cell can be empty (0), crossed (1), or circled (2);
+    let value = 0;
+
+    const getValue = () => value;
+
+    const addToken = (playerToken) => {
+        value = playerToken;
     };
 
-    const getPlayerChoice = () => {
-        return {Player.type, location};
+    return { getValue, addToken };
+}
+
+function GameFlow(name1 = 'player1', name2 = 'player2') {
+
+    const board = GameBoard();
+
+    const players = [
+        {
+            name: name1,
+            token: 1
+        },
+        {
+            name: name2,
+            token: 2
+        }
+    ];
+
+    let currentPlayer = players[0];
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const addTurn = () => {
+        if (currentPlayer === players[0]) currentPlayer = players[1];
+        else currentPlayer = players[0];
+    };
+
+    const printNewRound = () => {
+        board.displayBoard();
+        console.log(`It's ${currentPlayer.name}'s turn to play`);
+    };
+
+    const playRound = (row, column) => {
+
+        board.fillCell(currentPlayer.token, column, row);
+        
+        //Check if there is a winner, if not, continue with the following
+
+        addTurn();
+        printNewRound();
     };
 
     const checkGameOver = () => {
-        if () return true;
-        return false;
+        // Check for a winner or if the gameboard is full.
+        // Implement after the rest of the game is done
     };
-})();
 
-function createPlayer (type, first) {
-    return {type, first};
-}
+    printNewRound();
 
-const Cross = createPlayer('X', true);
-const Circle = createPlayer('O', false);
+    return {playRound, getCurrentPlayer};
+};
 
-console.log(Gameboard.board)
+const game = GameFlow();
